b753845d2ab344c5777f18e872e5cbf2412dc588
Will <w255li@edu.uwaterloo.ca>
2020-02-01 17:52:28 -0500

    anova analysis for baseline data

--- Baseline/baseline_anova.R
+++ Baseline/baseline_anova.R
@@ -1,5 +1,8 @@
 data <- read.delim("Baseline.csv")
 data$shift.day <- data$shift + (data$daycount-1)*3
+data$hour <- data$hour <- data$partnum %/% 60
 
-model <- lm(y300 ~ as.factor(shift.day) + as.factor(daycount) + as.factor(shift), data)
+model <- lm(y300 ~ as.factor(shift.day) + 
+              as.factor(hour), 
+            data)
 summary(aov(model))
