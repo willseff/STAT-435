e54d5c55b1d6389af044d6772d88699dae66b5d7
Will <w255li@edu.uwaterloo.ca>
2020-02-28 18:08:01 -0500

    added data from previous week to plots

--- DominantCauseSearch/x29_verification.R
+++ DominantCauseSearch/x29_verification.R
@@ -6,6 +6,12 @@ investigation.df <- read.delim("x29_prospective2.csv")
 # The experimental data
 verification.df <- read.delim("x29_verification.csv")
 
+# The data from the individual causes investigation
+individual.df <- read.delim("individual_causes.csv")
+
+individual.x29.df <- subset(individual.df, select = c("y200", "x29"))
+individual.x29.df$type <- c(rep("Prospective", nrow(individual.df)))
+
 plots.base <- ggplot() + 
   theme(plot.title=element_text(hjust=0.5, face="bold"), 
         axis.title=element_text(size=12))
@@ -18,3 +24,11 @@ both.df$type <- c(rep("Prospective", nrow(investigation.df)),
                   rep("Experimental", nrow(verification.df)))
 
 plots.base + geom_point(aes(x=x29, y=y200, color=type), both.df)
+
+# df of all data with x29 and y200
+all.df <- rbind(both.df,individual.x29.df)
+plots.base + geom_point(aes(x=x29, y=y200, color=type), all.df) +
+  geom_smooth(aes(x=x29, y=y200), method='lm', formula= y~x, all.df) + 
+  ggtitle('y200 vs x29(Power Level)')
+
+
