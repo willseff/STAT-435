3108816788459f458c548669c0e0595d77532eaa
Will <w255li@edu.uwaterloo.ca>
2020-03-07 11:38:26 -0500

    Update baseline.R

--- Baseline/baseline.R
+++ Baseline/baseline.R
@@ -1,19 +1,27 @@
 library(ggplot2)
 library(ggthemes)
 
-initial <- read.delim("~/Desktop/stat_435_take_home_midterm/baseline_initial.csv")
-initial2 <- read.delim("~/Desktop/stat_435_take_home_midterm/initial2.csv")
+initial <- read.delim("baseline_initial.csv")
+initial2 <- read.delim("initial2.csv")
 
+initial <- rbind(initial, initial2)
 initial$hour <- as.integer(initial$partnum/60)
-model <- lm(y300 ~ as.factor(hour), baseline.initial)
+model <- lm(y300 ~ as.factor(hour), initial)
 
 aov <- summary(aov(model))
 
-ggplot(baseline.initial, aes(x = hour, y = y300, group = hour)) +
+ggplot(initial, aes(x = hour, y = y300, group = hour)) +
   geom_boxplot()
 
-ggplot(baseline.initial, aes(x = hour, y = y300)) + geom_point()
+ggplot(initial, aes(x = hour, y = y300)) + geom_point()
 
+ggplot(initial, aes(x = partnum, y = y300)) + geom_point()
+
+initial$shift.min <- initial$partnum %% 240
+initial$shift.day <- initial$shift + (initial$daycount-1)*3
+model.shift.min <- lm(y300 ~ shift.min + as.factor(shift.day) , initial)
+summary(model.shift.min)
+summary(aov(model.shift.min))
 # pooled standard deviation
 sqrt(aov[1][[1]][[3]][2])
 # mean
@@ -23,7 +31,7 @@ mean(baseline.initial$y300)
 aggregate(baseline.initial[, 4], list(baseline.initial$hour), mean)
 
 # actual baseline study data
-baseline <- read.delim("~/Desktop/stat_435_take_home_midterm/baseline.csv")
+baseline <- read.delim("baseline.csv")
 # add initial study data to baseline df
 baseline <- rbind(initial,initial2,baseline)
 
@@ -37,15 +45,15 @@ summary(baseline)
 #histogram of all data
 ggplot(baseline, aes(x=y300)) + geom_histogram(bins=40) +
   ggtitle('Histogram of y300 Values') + 
-  theme_economist_white(gray_bg=FALSE) + 
-  scale_colour_economist()
+  theme_bw() + 
+  scale_shape_cleveland()
 
 # plot of all points
 ggplot(baseline, aes(x = partnum, y = y300)) + 
   geom_point() + 
   theme_bw() + 
   scale_shape_cleveland() + 
-  ggtitle('Plot of all data')
+  ggtitle('Plot of All data')
 
 # box plot by shift
 ggplot(baseline, aes(x = shift.day, y = y300, group = shift.day)) +
@@ -55,14 +63,19 @@ ggplot(baseline, aes(x = shift.day, y = y300, group = shift.day)) +
 
 #box plot by day
 ggplot(baseline, aes(x = daycount, y = y300, group = daycount)) +
-  geom_boxplot() + theme_bw() + scale_shape_cleveland() + ggtitle('Boxplot by Day')
+  geom_boxplot() + 
+  theme_bw() + 
+  scale_shape_cleveland() + 
+  ggtitle('Boxplot by Day')
 
 # dotplot by hour
 ggplot(baseline, aes(x = hour, y = y300, group = hour)) + 
   geom_dotplot(binaxis = "y", dotsize = 1, stackdir = "center", binwidth=0.25) + 
-  ggtitle("Dotplot by Hour")
+  ggtitle("Dotplot by Hour") +
+  theme_bw() + 
+  scale_shape_cleveland()
 
-# violinplot by day
+# violin plot by day
 ggplot(baseline, aes(x = daycount, y = y300, group = daycount)) + geom_violin() + 
   theme_bw() + 
   scale_shape_cleveland() +
@@ -70,6 +83,10 @@ ggplot(baseline, aes(x = daycount, y = y300, group = daycount)) + geom_violin() 
 
 # violin plot by shift
 ggplot(baseline, aes(x=shift.day, y=y300, group = shift.day)) + 
+  geom_violin()
+
+# violin plot by hour
+ggplot(baseline, aes(x=hour, y=y300, group=hour)) +
   geom_violin()
 
 max(baseline$y300)
@@ -82,10 +99,16 @@ summary(aov(nested.model))
 sd(baseline$y300)
 
 # minute analysis
-ggplot(baseline, aes(x=minute)) + geom_histogram(bins=40)
+ggplot(baseline, aes(x=minute)) + 
+  geom_histogram(bins=40) +
+  theme_bw() + 
+  scale_shape_cleveland() + 
+  ggtitle('Histogram of Minutes')
 
 ggplot(baseline, aes(x=minute,y=y300)) + geom_point() +
-  ggtitle('Part by minute')
+  theme_bw() + 
+  scale_shape_cleveland() + 
+  ggtitle('Part by Minute')
 
 model.minute <- lm(y300 ~ minute, baseline)
 summary(model.minute)
