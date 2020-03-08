aa3e8afd08102c377c5d690acbae517e0b13998a
Will <w255li@edu.uwaterloo.ca>
2020-03-08 12:11:41 -0400

    new plots added

--- Search for the Dominant Cause/y100baseline.R
+++ Search for the Dominant Cause/y100baseline.R
@@ -1,12 +1,24 @@
 baseline.y100 <- read.delim('y100baseline.csv')
+transmit<- read.delim('transmit.csv')
 
+transmit$y200 <- NULL
+transmit$y300 <- NULL
+
+baseline.y100 <- rbind(baseline.y100, transmit)
 # data engineering
 baseline.y100$hour <- as.integer(baseline.y100$partnum/60)
 baseline.y100$shift.day <- baseline.y100$shift + (baseline.y100$daycount-1)*3
 
+gg.base <- ggplot(baseline.y100)
+
 ggplot(baseline.y100, aes(x=partnum, y=y100)) + geom_point()
 ggplot(baseline.y100, aes(x=shift.day, y=y100)) + geom_point()
 
-nested.model <- lm(y100 ~ as.factor(daycount)/as.factor(shift.day)/as.factor(hour), baseline.y100)
+gg.base + geom_boxplot(aes(x=daycount, y=y100, group=(daycount)))
+
+ggplot(baseline.y100, aes(x=shift.day, y=y100, group = shift.day)) + geom_boxplot()
+
+nested.model <- lm(y100 ~ as.factor(daycount)/as.factor(shift.day), baseline.y100)
 summary(aov(nested.model))
+
 
