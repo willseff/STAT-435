fea56ce4e65d3f1738cc9213041195b3ffab11da
Will <w255li@edu.uwaterloo.ca>
2020-03-22 13:39:01 -0400

    Update process_center.R

--- ProcessCenter/process_center.R
+++ ProcessCenter/process_center.R
@@ -13,6 +13,24 @@ ggplot(data=fact.df) + geom_point(aes(x=z7, y=y200, color=as.factor(z8)))
 
 ggplot(data=fact.df) + geom_point(aes(x=z7, y=y200, color=as.factor(z9)))
 
-ggplot(data=fact.df) + geom_point(aes(x=z7, y=y200, color=as.factor(z10)))
+ggplot(data=fact.df) + geom_point(aes(x=z8, y=y200, color=as.factor(z9)))
 
 ggplot(data=fact.df) + geom_point(aes(x=z10, y=y200, color=as.factor(z8)))
+
+
+attach(fact.df)
+
+par(mfrow=c(2,3))
+
+interaction.plot(z7, z8, y200)
+interaction.plot(z7, z9, y200)
+interaction.plot(z7, z10, y200)
+interaction.plot(z8, z9, y200)
+interaction.plot(z8, z10, y200)
+interaction.plot(z9, z10, y200)
+
+fact.model <- aov(y200 ~ as.factor(z7) + as.factor(z8) + as.factor(z9) + as.factor(z10) + 
+                    as.factor(z10)*as.factor(z7) + as.factor(z8)*as.factor(z9) , 
+                  data=fact.df)
+summary(fact.model)
+
