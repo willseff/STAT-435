5618e0c33c1b0e28f00f21936468b5dccdd11de9
Will <w255li@edu.uwaterloo.ca>
2020-03-21 21:06:16 -0400

    Revert "new plots added"
    This reverts commit aa3e8afd08102c377c5d690acbae517e0b13998a.

--- Search for the Dominant Cause/transmit.R
+++ Search for the Dominant Cause/transmit.R
@@ -3,31 +3,8 @@ transmit <- read.delim('transmit.csv')
 summary(transmit)
 
 # plots of data
-gg.base <- ggplot(transmit) + theme_bw() + scale_shape_cleveland() 
-
-# function for getting linear regression equation
-lm_eqn <- function(m){
-  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                   list(a = format(coef(m)[1], digits = 2), 
-                        b = format(coef(m)[2], digits = 2), 
-                        r2 = format(summary(m)$r.squared, digits = 3)))
-  as.character(as.expression(eq));                 
-}
-
-# plot y300 vs y200
-gg.base + geom_point(aes(x=y200,y=y300)) +
-  ggtitle('Plot of y300 vs y200') +
-  geom_smooth(aes(x=y200,y=y300),method='lm', formula= y~x) +
-  geom_text(x = 1.5, y = 9, label = lm_eqn(lm(y300~y200, transmit)), parse = TRUE
-            , size = 5)
-
-# plot of y200 vs y100
-gg.base + geom_point(aes(x=y100,y=y200)) +
-  ggtitle('Plot of y200 vs y100') +
-  geom_smooth(aes(x=y100,y=y200),method='lm', formula= y~x) +
-  geom_text(x = 7, y = 0, label = lm_eqn(lm(y200~y100, transmit)), parse = TRUE
-            , size = 5)
-
+ggplot(transmit, aes(x=y200,y=y300)) + geom_point()
+summary(transmit$y300)
 
 model1 <- lm(y300 ~ y200, transmit)
 summary(model1)
